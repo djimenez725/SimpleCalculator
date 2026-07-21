ce7b44be3bc013cd0d6b1fe8aeba25be00338d80
djimenez725 <dianerisj@gmail.com>
2026-07-21 10:53:00 -0400

    Fix division logic and update display assignment

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const deletebutton = document.getElementById("deleteBtn");
 deletebutton.addEventListener("click", () => {
   display.value = display.value.toString().slice(0, -1);
 });
+});
 
 function appendToDisplay(value) {
     d.value += value;
@@ -56,14 +57,18 @@ function evaluateExpression() {
         result = parseFloat(number[0]) * parseFloat(number[1]);
     } 
     
-    else if (/\//.test(exp)) {
-        number = exp.split("/");
-        if (number[1] === "0") {
-            result = "Error";
-        } else {
-            result = parseFloat(number[0]) / parseFloat(number[1]);
-        }
+ else if (/\//.test(exp)) {
+    number = exp.split("/");
 
-        d.value = result;
+    if (number[1] === "0") {
+        result = "Error";
+    } else {
+        result = parseFloat(number[0]) / parseFloat(number[1]);
+    }
 }
+
+d.value = result;
+} 
+
+});
 
